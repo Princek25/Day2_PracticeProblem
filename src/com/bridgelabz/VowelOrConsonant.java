ac3afe6a94f01b6397ca3626b1b8d2d81953935c
Prince <princek2530@gmail.com>
2022-10-13 14:31:08 +0530

    Added: Program to Check Vowel or Consonant

--- src/com/bridgelabz/VowelOrConsonant.java
+++ src/com/bridgelabz/VowelOrConsonant.java
@@ -1,2 +1,28 @@
-package com.bridgelabz;public class VowelOrConsonant {
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+    static void checkVowel(String s){
+        switch (s){
+            case "a" : System.out.println("Vowel"); break;
+            case "i" : System.out.println("Vowel"); break;
+            case "o" : System.out.println("Vowel"); break;
+            case "u" : System.out.println("Vowel"); break;
+            case "e" : System.out.println("Vowel"); break;
+            case "A" : System.out.println("Vowel"); break;
+            case "I" : System.out.println("Vowel"); break;
+            case "O" : System.out.println("Vowel"); break;
+            case "U" : System.out.println("Vowel"); break;
+            case "E" : System.out.println("Vowel"); break;
+            default  : System.out.println("consonant");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a letter from a to z or A to Z");
+        String s = sc.next();
+        checkVowel(s);
+
+    }
 }
